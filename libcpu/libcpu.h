ergo720/libcpu

commit 2bd612f86e8453426c7260175235c85265603829

Renames and simplifications regarding 'regclass'.

libcpu/libcpu.h
@@ -101,13 +101,14 @@ enum {
 // @@@BEGIN_DEPRECATION
 // Four register classes
 enum {
-	CPU_REG_GPR, // General Purpose
-	CPU_REG_FPR, // Floating Point
-	CPU_REG_VR,  // Vector
-	CPU_REG_XR   // Extra Registers, the core expects these to follow
-	 			 // GPRs in the memory layout, they are kept separate
-				 // to avoid confusing the client about the number of
-				 // registers available.
+	CPU_REGCLASS_GPR, // General Purpose
+	CPU_REGCLASS_FPR, // Floating Point
+	CPU_REGCLASS_VR,  // Vector
+	CPU_REGCLASS_XR,  // Extra Registers, the core expects these to follow
+	 				  // GPRs in the memory layout, they are kept separate
+					  // to avoid confusing the client about the number of
+					  // registers available.
+	CPU_REGCLASS_COUNT
 };
 // @@@END_DEPRECATION
 
@@ -182,8 +183,8 @@ typedef struct cpu_archinfo {
 	uint32_t default_page_size;
 
 	cpu_register_layout_t const *register_layout;
-	uint32_t register_count[4];
-	uint32_t register_offset[4];
+	size_t regclass_count[CPU_REGCLASS_COUNT];
+	size_t regclass_offset[CPU_REGCLASS_COUNT];
 	uint32_t register_count2;
 	cpu_flags_layout_t const *flags_layout;
 	uint32_t flags_count;

arch/6502/6502_arch.cpp
@@ -50,9 +50,9 @@ arch_6502_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 	info->address_size = 16;
 	info->psr_size = 8;
 	// There are 4 8-bit GPRs
-	info->register_count[CPU_REG_GPR] = 4;
+	info->regclass_count[CPU_REGCLASS_GPR] = 4;
 	// There is also 1 extra register to handle PSR.
-	info->register_count[CPU_REG_XR] = 1;
+	info->regclass_count[CPU_REGCLASS_XR] = 1;
 	info->register_layout = arch_6502_reg_layout;
 
 	info->flags_count = 8;

arch/arm/arm_arch.cpp
@@ -52,9 +52,9 @@ arch_arm_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 	info->float_size = 64;
 	info->address_size = 32;
 	// There are 16 32-bit GPRs
-	info->register_count[CPU_REG_GPR] = 16;
+	info->regclass_count[CPU_REGCLASS_GPR] = 16;
 	// There is also 1 extra register to handle PSR.
-	info->register_count[CPU_REG_XR] = 1;
+	info->regclass_count[CPU_REGCLASS_XR] = 1;
 	info->register_layout = arch_arm_reg_layout;
 
 	reg_arm_t *reg;

arch/fapra/fapra_arch.cpp
@@ -58,7 +58,7 @@ arch_fapra_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 	info->max_page_size = 16777216;
 	info->default_page_size = 4096;
 	// There are 32 32-bit GPRs 
-	info->register_count[CPU_REG_GPR] = 32;
+	info->regclass_count[CPU_REGCLASS_GPR] = 32;
 	info->register_layout = arch_fapra_reg_layout;
 
 	reg_fapra32_t *reg;

arch/m68k/m68k_arch.cpp
@@ -45,7 +45,7 @@ arch_m68k_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 	info->max_page_size = 8192;
 	info->default_page_size = 8192;
 	// There are 16 32-bit GPRs 
-	info->register_count[CPU_REG_GPR] = 16;
+	info->regclass_count[CPU_REGCLASS_GPR] = 16;
 	info->register_layout = arch_m68k_reg_layout;
 
 	reg_m68k_t *reg;

arch/m88k/m88k_arch.cpp
@@ -112,11 +112,11 @@ arch_m88k_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 	info->max_page_size = 4096;
 	info->default_page_size = 4096;
 	// There are 32 32-bit GPRs and 32 80-bit FPRs.
-	info->register_count[CPU_REG_GPR] = 32;
-	info->register_count[CPU_REG_FPR] = 32;
+	info->regclass_count[CPU_REGCLASS_GPR] = 32;
+	info->regclass_count[CPU_REGCLASS_FPR] = 32;
 	// There are also 2 extra registers to handle
 	// PSR and TRAPNO.
-	info->register_count[CPU_REG_XR] = 2;
+	info->regclass_count[CPU_REGCLASS_XR] = 2;
 	info->register_layout = arch_m88k_reg_layout;
 
 	// Setup the register files

arch/mips/mips_arch.cpp
@@ -112,9 +112,9 @@ arch_mips_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 	info->max_page_size = 16777216;
 	info->default_page_size = 4096;
 	// There are 32 32-bit GPRs 
-	info->register_count[CPU_REG_GPR] = 32;
+	info->regclass_count[CPU_REGCLASS_GPR] = 32;
 	// There are 2 extra registers, HI/LO for MUL/DIV insn.
-	info->register_count[CPU_REG_XR] = 2;
+	info->regclass_count[CPU_REGCLASS_XR] = 2;
 	info->register_layout = arch_mips_reg_layout;
 
 	if (info->arch_flags & CPU_MIPS_IS_64BIT) {

arch/x86/x86_arch.cpp
@@ -86,9 +86,9 @@ arch_x86_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 	info->address_size = 32;
 	info->psr_size = 32;
 
-	info->register_count[CPU_REG_GPR] = 8;
-	info->register_count[CPU_REG_FPR] = 8;
-	info->register_count[CPU_REG_XR] = 17;
+	info->regclass_count[CPU_REGCLASS_GPR] = 8;
+	info->regclass_count[CPU_REGCLASS_FPR] = 8;
+	info->regclass_count[CPU_REGCLASS_XR] = 17;
 	info->register_layout = arch_x86_reg_layout;
 
 	info->flags_count = 18;

commit f99d8b4a3b4cf24c97859b937e5bb73e6ca50253

Fix llvm assertion introduced in 2bd612f86e8453426c7260175235c85265603829

libcpu/libcpu.h
@@ -102,12 +102,12 @@ enum {
 // Four register classes
 enum {
 	CPU_REGCLASS_GPR, // General Purpose
+	CPU_REGCLASS_XR,  // Extra Registers, the core expects these to follow
+				  // GPRs in the memory layout, they are kept separate
+				  // to avoid confusing the client about the number of
+				  // registers available.
 	CPU_REGCLASS_FPR, // Floating Point
 	CPU_REGCLASS_VR,  // Vector
-	CPU_REGCLASS_XR,  // Extra Registers, the core expects these to follow
-	 				  // GPRs in the memory layout, they are kept separate
-					  // to avoid confusing the client about the number of
-					  // registers available.
 	CPU_REGCLASS_COUNT
 };
 // @@@END_DEPRECATION