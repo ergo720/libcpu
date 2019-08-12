ergo720/libcpu

commit 57455fc813a3d63ae93ebdbd3b4013c03896498f

Registers can have different sizes now (untested)

arch/x86/x86_arch.cpp
@@ -11,6 +11,42 @@
 #include "frontend.h"
 #include "x86_internal.h"
 
+static cpu_register_layout_t arch_x86_reg_layout[] = {
+	{ 0, 32, 0, 0, 0, "EAX" },
+	{ 0, 32, 0, 0, 0, "ECX" },
+	{ 0, 32, 0, 0, 0, "EDX" },
+	{ 0, 32, 0, 0, 0, "EBX" },
+	{ 0, 32, 0, 0, 0, "ESP" },
+	{ 0, 32, 0, 0, 0, "EBP" },
+	{ 0, 32, 0, 0, 0, "ESI" },
+	{ 0, 32, 0, 0, 0, "EDI" },
+	{ 0, 32, 0, 0, 0, "EFLAGS" },
+	{ 0, 16, 0, 0, 0, "ES" },
+	{ 0, 16, 0, 0, 0, "CS" },
+	{ 0, 16, 0, 0, 0, "SS" },
+	{ 0, 16, 0, 0, 0, "DS" },
+	{ 0, 16, 0, 0, 0, "FS" },
+	{ 0, 16, 0, 0, 0, "GS" },
+	{ 0, 32, 0, 0, 0, "CR0" },
+	{ 0, 32, 0, 0, 0, "CR2" },
+	{ 0, 32, 0, 0, 0, "CR3" },
+	{ 0, 32, 0, 0, 0, "CR4" },
+	{ 0, 32, 0, 0, 0, "DR0" },
+	{ 0, 32, 0, 0, 0, "DR1" },
+	{ 0, 32, 0, 0, 0, "DR2" },
+	{ 0, 32, 0, 0, 0, "DR3" },
+	{ 0, 32, 0, 0, 0, "DR6" },
+	{ 0, 32, 0, 0, 0, "DR7" },
+	{ 0, 80, 0, 0, 0, "ST0" },
+	{ 0, 80, 0, 0, 0, "ST1" },
+	{ 0, 80, 0, 0, 0, "ST2" },
+	{ 0, 80, 0, 0, 0, "ST3" },
+	{ 0, 80, 0, 0, 0, "ST4" },
+	{ 0, 80, 0, 0, 0, "ST5" },
+	{ 0, 80, 0, 0, 0, "ST6" },
+	{ 0, 80, 0, 0, 0, "ST7" },
+};
+
 static cpu_flags_layout_t arch_x86_flags_layout[] = {
 	{ ID_SHIFT, 0, "ID" }, /* cpuid supported */
 	{ VIP_SHIFT, 0, "VIP" }, /* virtual interrupt pending */
@@ -51,13 +87,9 @@ arch_x86_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 	info->psr_size = 32;
 
 	info->register_count[CPU_REG_GPR] = 8;
-	info->register_size[CPU_REG_GPR] = info->word_size;
-
 	info->register_count[CPU_REG_FPR] = 8;
-	info->register_size[CPU_REG_FPR] = info->float_size;
-
-	info->register_count[CPU_REG_XR] = 1;
-	info->register_size[CPU_REG_XR] = info->word_size;
+	info->register_count[CPU_REG_XR] = 17;
+	info->register_layout = arch_x86_reg_layout;
 
 	info->flags_count = 18;
 	info->flags_layout = arch_x86_flags_layout;
@@ -105,34 +137,10 @@ arch_x86_get_psr(cpu_t *, void *reg)
 static int
 arch_x86_get_reg(cpu_t *cpu, void *reg, unsigned reg_no, uint64_t *value)
 {
-	switch (reg_no) {
-	case 0:
-		*value = ((reg_x86_t*)reg)->eax;
-		break;
-	case 1:
-		*value = ((reg_x86_t*)reg)->ecx;
-		break;
-	case 2:
-		*value = ((reg_x86_t*)reg)->edx;
-		break;
-	case 3:
-		*value = ((reg_x86_t*)reg)->ebx;
-		break;
-	case 4:
-		*value = ((reg_x86_t*)reg)->esp;
-		break;
-	case 5:
-		*value = ((reg_x86_t*)reg)->ebp;
-		break;
-	case 6:
-		*value = ((reg_x86_t*)reg)->esi;
-		break;
-	case 7:
-		*value = ((reg_x86_t*)reg)->edi;
-		break;
-	default:
+	if (reg_no > 8)
 		return -1;
-	}
+
+	*value = ((reg_x86_t*)reg)->gpr[reg_no];
 	return 0;
 }
 

arch/x86/x86_types.h
@@ -43,16 +43,21 @@
 
 PACKED(struct reg_x86_s {
 	/* General registers */
-	DEFINE_SPLIT_REG8(eax, ax, ah, al);
-	DEFINE_SPLIT_REG8(ecx, cx, ch, cl);
-	DEFINE_SPLIT_REG8(edx, dx, dh, dl);
-	DEFINE_SPLIT_REG8(ebx, bx, bh, bl);
-	/* Pointer registers */
-	DEFINE_SPLIT_REG16(esp, sp);
-	DEFINE_SPLIT_REG16(ebp, bp);
-	/* Index registers */
-	DEFINE_SPLIT_REG16(esi, si);
-	DEFINE_SPLIT_REG16(edi, di);
+	union {
+		struct {
+			DEFINE_SPLIT_REG8(eax, ax, ah, al);
+			DEFINE_SPLIT_REG8(ecx, cx, ch, cl);
+			DEFINE_SPLIT_REG8(edx, dx, dh, dl);
+			DEFINE_SPLIT_REG8(ebx, bx, bh, bl);
+			/* Pointer registers */
+			DEFINE_SPLIT_REG16(esp, sp);
+			DEFINE_SPLIT_REG16(ebp, bp);
+			/* Index registers */
+			DEFINE_SPLIT_REG16(esi, si);
+			DEFINE_SPLIT_REG16(edi, di);
+		};
+		uint32_t gpr[8];
+	};
 	/* Eflags register */
 	DEFINE_REG32(eflags);
 	/* Special purpose registers */

libcpu/interface.cpp
@@ -38,36 +38,48 @@ extern arch_func_t arch_func_fapra;
 #define IS_LITTLE_ENDIAN(cpu) (((cpu)->info.common_flags & CPU_FLAG_ENDIAN_MASK) == CPU_FLAG_ENDIAN_LITTLE)
 
 static inline bool
-is_valid_gpr_size(size_t size)
+is_valid_gpr_size(cpu_t *cpu, uint32_t offset, uint32_t count)
 {
-	switch (size) {
+	for (; offset < count; offset++) {
+		size_t size = cpu->info.register_layout[offset].bits_size;
+		switch (size) {
 		case 0: case 1: case 8: case 16: case 32: case 64:
-			return true;
+			break;
 		default:
 			return false;
+		}
 	}
+	return true;
 }
 
 static inline bool
-is_valid_fpr_size(size_t size)
+is_valid_fpr_size(cpu_t *cpu, uint32_t offset, uint32_t count)
 {
-	switch (size) {
+	for (; offset < count; offset++) {
+		size_t size = cpu->info.register_layout[offset].bits_size;
+		switch (size) {
 		case 0: case 32: case 64: case 80: case 128:
-			return true;
+			break;
 		default:
 			return false;
+		}
 	}
+	return true;
 }
 
 static inline bool
-is_valid_vr_size(size_t size)
+is_valid_vr_size(cpu_t *cpu, uint32_t offset, uint32_t count)
 {
-	switch (size) {
+	for (; offset < count; offset++) {
+		size_t size = cpu->info.register_layout[offset].bits_size;
+		switch (size) {
 		case 0: case 64: case 128:
-			return true;
+			break;
 		default:
 			return false;
+		}
 	}
+	return true;
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -136,14 +148,24 @@ cpu_new(cpu_arch_t arch, uint32_t flags, uint32_t arch_flags)
 	// init the frontend
 	cpu->f.init(cpu, &cpu->info, &cpu->rf);
 
-	assert(is_valid_gpr_size(cpu->info.register_size[CPU_REG_GPR]) &&
+	size_t offset = 0;
+	cpu->info.register_offset[CPU_REG_GPR] = offset;
+	offset += cpu->info.register_count[CPU_REG_GPR];
+	cpu->info.register_offset[CPU_REG_XR] = offset;
+	offset += cpu->info.register_count[CPU_REG_XR];
+	cpu->info.register_offset[CPU_REG_FPR] = offset;
+	offset += cpu->info.register_count[CPU_REG_FPR];
+	cpu->info.register_offset[CPU_REG_VR] = offset;
+
+	assert(is_valid_gpr_size(cpu, offset, cpu->info.register_offset[CPU_REG_GPR]) &&
 		"the specified GPR size is not guaranteed to work");
-	assert(is_valid_fpr_size(cpu->info.register_size[CPU_REG_FPR]) &&
-		"the specified FPR size is not guaranteed to work");
-	assert(is_valid_vr_size(cpu->info.register_size[CPU_REG_VR]) &&
-		"the specified VR size is not guaranteed to work");
-	assert(is_valid_gpr_size(cpu->info.register_size[CPU_REG_XR]) &&
+	assert(is_valid_gpr_size(cpu, offset, cpu->info.register_offset[CPU_REG_XR]) &&
 		"the specified XR size is not guaranteed to work");
+	assert(is_valid_fpr_size(cpu, offset, cpu->info.register_offset[CPU_REG_FPR]) &&
+		"the specified FPR size is not guaranteed to work");
+	assert(is_valid_vr_size(cpu, offset, cpu->info.register_offset[CPU_REG_VR]) &&
+		"the specified VR size is not guaranteed to work");
+
 
 	uint32_t count = cpu->info.register_count[CPU_REG_GPR];
 	if (count != 0) {

arch/6502/6502_arch.cpp
@@ -12,6 +12,14 @@
 #include "frontend.h"
 #include "6502_internal.h"
 
+static cpu_register_layout_t arch_6502_reg_layout[] = {
+	{ 0, 8, 0, 0, 0, "X" },
+	{ 0, 8, 0, 0, 0, "Y" },
+	{ 0, 8, 0, 0, 0, "S" },
+	{ 0, 8, 0, 0, 0, "A" },
+	{ 0, 8, 0, 0, 0, "P" },
+};
+
 static cpu_flags_layout_t arch_6502_flags_layout[] = {
 	{ N_SHIFT, CPU_FLAGTYPE_NEGATIVE, "N" },	/* negative */
 	{ V_SHIFT, CPU_FLAGTYPE_OVERFLOW, "V" },	/* overflow */
@@ -43,10 +51,9 @@ arch_6502_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 	info->psr_size = 8;
 	// There are 4 8-bit GPRs
 	info->register_count[CPU_REG_GPR] = 4;
-	info->register_size[CPU_REG_GPR] = info->word_size;
 	// There is also 1 extra register to handle PSR.
 	info->register_count[CPU_REG_XR] = 1;
-	info->register_size[CPU_REG_XR] = 8;
+	info->register_layout = arch_6502_reg_layout;
 
 	info->flags_count = 8;
 	info->flags_layout = arch_6502_flags_layout;

arch/arm/arm_arch.cpp
@@ -11,6 +11,26 @@
 #include "arm_internal.h"
 #include "frontend.h"
 
+static cpu_register_layout_t arch_arm_reg_layout[] = {
+	{ 0, 32, 0, 0, 0, "R0" },
+	{ 0, 32, 0, 0, 0, "R1" },
+	{ 0, 32, 0, 0, 0, "R2" },
+	{ 0, 32, 0, 0, 0, "R3" },
+	{ 0, 32, 0, 0, 0, "R4" },
+	{ 0, 32, 0, 0, 0, "R5" },
+	{ 0, 32, 0, 0, 0, "R6" },
+	{ 0, 32, 0, 0, 0, "R7" },
+	{ 0, 32, 0, 0, 0, "R8" },
+	{ 0, 32, 0, 0, 0, "R9" },
+	{ 0, 32, 0, 0, 0, "R10" },
+	{ 0, 32, 0, 0, 0, "R11" },
+	{ 0, 32, 0, 0, 0, "R12" },
+	{ 0, 32, 0, 0, 0, "R13" },
+	{ 0, 32, 0, 0, 0, "R14" },
+	{ 0, 32, 0, 0, 0, "R15" },
+	{ 0, 32, 0, 0, 0, "CPSR" },
+};
+
 static void
 arch_arm_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 {
@@ -33,10 +53,9 @@ arch_arm_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 	info->address_size = 32;
 	// There are 16 32-bit GPRs
 	info->register_count[CPU_REG_GPR] = 16;
-	info->register_size[CPU_REG_GPR] = info->word_size;
 	// There is also 1 extra register to handle PSR.
 	info->register_count[CPU_REG_XR] = 1;
-	info->register_size[CPU_REG_XR] = 32;
+	info->register_layout = arch_arm_reg_layout;
 
 	reg_arm_t *reg;
 	reg = (reg_arm_t*)malloc(sizeof(reg_arm_t));

arch/fapra/fapra_arch.cpp
@@ -3,6 +3,41 @@
 #include "fapra_interface.h"
 #include "frontend.h"
 
+static cpu_register_layout_t arch_fapra_reg_layout[] = {
+	{ 0, 32, 0, 0, 0, "R0" },
+	{ 0, 32, 0, 0, 0, "R1" },
+	{ 0, 32, 0, 0, 0, "R2" },
+	{ 0, 32, 0, 0, 0, "R3" },
+	{ 0, 32, 0, 0, 0, "R4" },
+	{ 0, 32, 0, 0, 0, "R5" },
+	{ 0, 32, 0, 0, 0, "R6" },
+	{ 0, 32, 0, 0, 0, "R7" },
+	{ 0, 32, 0, 0, 0, "R8" },
+	{ 0, 32, 0, 0, 0, "R9" },
+	{ 0, 32, 0, 0, 0, "R10" },
+	{ 0, 32, 0, 0, 0, "R11" },
+	{ 0, 32, 0, 0, 0, "R12" },
+	{ 0, 32, 0, 0, 0, "R13" },
+	{ 0, 32, 0, 0, 0, "R14" },
+	{ 0, 32, 0, 0, 0, "R15" },
+	{ 0, 32, 0, 0, 0, "R16" },
+	{ 0, 32, 0, 0, 0, "R17" },
+	{ 0, 32, 0, 0, 0, "R18" },
+	{ 0, 32, 0, 0, 0, "R19" },
+	{ 0, 32, 0, 0, 0, "R20" },
+	{ 0, 32, 0, 0, 0, "R21" },
+	{ 0, 32, 0, 0, 0, "R22" },
+	{ 0, 32, 0, 0, 0, "R23" },
+	{ 0, 32, 0, 0, 0, "R24" },
+	{ 0, 32, 0, 0, 0, "R25" },
+	{ 0, 32, 0, 0, 0, "R26" },
+	{ 0, 32, 0, 0, 0, "R27" },
+	{ 0, 32, 0, 0, 0, "R28" },
+	{ 0, 32, 0, 0, 0, "R29" },
+	{ 0, 32, 0, 0, 0, "R30" },
+	{ 0, 32, 0, 0, 0, "R31" },
+};
+
 static void
 arch_fapra_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 {
@@ -24,7 +59,7 @@ arch_fapra_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 	info->default_page_size = 4096;
 	// There are 32 32-bit GPRs 
 	info->register_count[CPU_REG_GPR] = 32;
-	info->register_size[CPU_REG_GPR] = info->word_size;
+	info->register_layout = arch_fapra_reg_layout;
 
 	reg_fapra32_t *reg;
 	reg = (reg_fapra32_t *) malloc(sizeof(reg_fapra32_t));

arch/m68k/m68k_arch.cpp
@@ -2,6 +2,26 @@
 #include "m68k_internal.h"
 #include "frontend.h"
 
+static cpu_register_layout_t arch_m68k_reg_layout[] = {
+	{ 0, 32, 0, 0, 0, "R0" },
+	{ 0, 32, 0, 0, 0, "R1" },
+	{ 0, 32, 0, 0, 0, "R2" },
+	{ 0, 32, 0, 0, 0, "R3" },
+	{ 0, 32, 0, 0, 0, "R4" },
+	{ 0, 32, 0, 0, 0, "R5" },
+	{ 0, 32, 0, 0, 0, "R6" },
+	{ 0, 32, 0, 0, 0, "R7" },
+	{ 0, 32, 0, 0, 0, "R8" },
+	{ 0, 32, 0, 0, 0, "R9" },
+	{ 0, 32, 0, 0, 0, "R10" },
+	{ 0, 32, 0, 0, 0, "R11" },
+	{ 0, 32, 0, 0, 0, "R12" },
+	{ 0, 32, 0, 0, 0, "R13" },
+	{ 0, 32, 0, 0, 0, "R14" },
+	{ 0, 32, 0, 0, 0, "R15" },
+	//{ 0, 32, 0, 0, 0, "PSR" },
+};
+
 static void
 arch_m68k_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 {
@@ -26,7 +46,7 @@ arch_m68k_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 	info->default_page_size = 8192;
 	// There are 16 32-bit GPRs 
 	info->register_count[CPU_REG_GPR] = 16;
-	info->register_size[CPU_REG_GPR] = info->word_size;
+	info->register_layout = arch_m68k_reg_layout;
 
 	reg_m68k_t *reg;
 	reg = (reg_m68k_t*)malloc(sizeof(reg_m68k_t));

arch/m88k/m88k_arch.cpp
@@ -10,6 +10,75 @@
 #define ptr_TRAPNO	ptr_xr[1]
 #define ptr_C		(cpu->feptr)
 
+static cpu_register_layout_t arch_m88k_reg_layout[] = {
+	{ 0, 32, 0, 0, 0, "R0" },
+	{ 0, 32, 0, 0, 0, "R1" },
+	{ 0, 32, 0, 0, 0, "R2" },
+	{ 0, 32, 0, 0, 0, "R3" },
+	{ 0, 32, 0, 0, 0, "R4" },
+	{ 0, 32, 0, 0, 0, "R5" },
+	{ 0, 32, 0, 0, 0, "R6" },
+	{ 0, 32, 0, 0, 0, "R7" },
+	{ 0, 32, 0, 0, 0, "R8" },
+	{ 0, 32, 0, 0, 0, "R9" },
+	{ 0, 32, 0, 0, 0, "R10" },
+	{ 0, 32, 0, 0, 0, "R11" },
+	{ 0, 32, 0, 0, 0, "R12" },
+	{ 0, 32, 0, 0, 0, "R13" },
+	{ 0, 32, 0, 0, 0, "R14" },
+	{ 0, 32, 0, 0, 0, "R15" },
+	{ 0, 32, 0, 0, 0, "R16" },
+	{ 0, 32, 0, 0, 0, "R17" },
+	{ 0, 32, 0, 0, 0, "R18" },
+	{ 0, 32, 0, 0, 0, "R19" },
+	{ 0, 32, 0, 0, 0, "R20" },
+	{ 0, 32, 0, 0, 0, "R21" },
+	{ 0, 32, 0, 0, 0, "R22" },
+	{ 0, 32, 0, 0, 0, "R23" },
+	{ 0, 32, 0, 0, 0, "R24" },
+	{ 0, 32, 0, 0, 0, "R25" },
+	{ 0, 32, 0, 0, 0, "R26" },
+	{ 0, 32, 0, 0, 0, "R27" },
+	{ 0, 32, 0, 0, 0, "R28" },
+	{ 0, 32, 0, 0, 0, "R29" },
+	{ 0, 32, 0, 0, 0, "R30" },
+	{ 0, 32, 0, 0, 0, "R31" },
+	{ 0, 32, 0, 0, 0, "PSR" },
+	{ 0, 32, 0, 0, 0, "TRAPNO" },
+	{ 0, 80, 0, 0, 0, "X0" },
+	{ 0, 80, 0, 0, 0, "X1" },
+	{ 0, 80, 0, 0, 0, "X2" },
+	{ 0, 80, 0, 0, 0, "X3" },
+	{ 0, 80, 0, 0, 0, "X4" },
+	{ 0, 80, 0, 0, 0, "X5" },
+	{ 0, 80, 0, 0, 0, "X6" },
+	{ 0, 80, 0, 0, 0, "X7" },
+	{ 0, 80, 0, 0, 0, "X8" },
+	{ 0, 80, 0, 0, 0, "X9" },
+	{ 0, 80, 0, 0, 0, "X10" },
+	{ 0, 80, 0, 0, 0, "X11" },
+	{ 0, 80, 0, 0, 0, "X12" },
+	{ 0, 80, 0, 0, 0, "X13" },
+	{ 0, 80, 0, 0, 0, "X14" },
+	{ 0, 80, 0, 0, 0, "X15" },
+	{ 0, 80, 0, 0, 0, "X16" },
+	{ 0, 80, 0, 0, 0, "X17" },
+	{ 0, 80, 0, 0, 0, "X18" },
+	{ 0, 80, 0, 0, 0, "X19" },
+	{ 0, 80, 0, 0, 0, "X20" },
+	{ 0, 80, 0, 0, 0, "X21" },
+	{ 0, 80, 0, 0, 0, "X22" },
+	{ 0, 80, 0, 0, 0, "X23" },
+	{ 0, 80, 0, 0, 0, "X24" },
+	{ 0, 80, 0, 0, 0, "X25" },
+	{ 0, 80, 0, 0, 0, "X26" },
+	{ 0, 80, 0, 0, 0, "X27" },
+	{ 0, 80, 0, 0, 0, "X28" },
+	{ 0, 80, 0, 0, 0, "X29" },
+	{ 0, 80, 0, 0, 0, "X30" },
+	{ 0, 80, 0, 0, 0, "X31" },
+};
+
 static void
 arch_m88k_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 {
@@ -44,13 +113,11 @@ arch_m88k_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 	info->default_page_size = 4096;
 	// There are 32 32-bit GPRs and 32 80-bit FPRs.
 	info->register_count[CPU_REG_GPR] = 32;
-	info->register_size[CPU_REG_GPR] = info->word_size;
 	info->register_count[CPU_REG_FPR] = 32;
-	info->register_size[CPU_REG_FPR] = info->float_size;
 	// There are also 2 extra registers to handle
 	// PSR and TRAPNO.
 	info->register_count[CPU_REG_XR] = 2;
-	info->register_size[CPU_REG_XR] = 32;
+	info->register_layout = arch_m88k_reg_layout;
 
 	// Setup the register files
 	reg = (m88k_grf_t *)malloc(sizeof(m88k_grf_t));

commit 9575e9afbdd0845cc11870d6d2dea988ee13e4ea

Fix infinite loop introduced in 57455fc813a3d63ae93ebdbd3b4013c03896498f

arch/x86/x86_arch.cpp
@@ -90,8 +90,8 @@ arch_x86_init(cpu_t *cpu, cpu_archinfo_t *info, cpu_archrf_t *rf)
 	info->psr_size = 32;
 
 	info->regclass_count[CPU_REGCLASS_GPR] = 8;
-	info->regclass_count[CPU_REGCLASS_FPR] = 8;
 	info->regclass_count[CPU_REGCLASS_XR] = 20;
+	info->regclass_count[CPU_REGCLASS_FPR] = 8;
 	info->register_layout = arch_x86_register_layout;
 
 	info->flags_count = 18;

arch/x86/x86_types.h
@@ -60,8 +60,6 @@ PACKED(struct reg_x86_s {
 	};
 	/* Eflags register */
 	DEFINE_REG32(eflags);
-	/* Special purpose registers */
-	DEFINE_REG32(eip);
 	/* Segment registers */
 	DEFINE_REG16(cs);
 	DEFINE_REG16(ds);
@@ -75,7 +73,18 @@ PACKED(struct reg_x86_s {
 	DEFINE_REG32(cr2);
 	DEFINE_REG32(cr3);
 	DEFINE_REG32(cr4);
-	// TODO: GDTR, LDTR, IDTR, task, MTRR, MSR, MMX, SSE, debug regs
+	/* Debug registers */
+	DEFINE_REG32(dr0);
+	DEFINE_REG32(dr1);
+	DEFINE_REG32(dr2);
+	DEFINE_REG32(dr3);
+	DEFINE_REG32(dr4);
+	DEFINE_REG32(dr5);
+	DEFINE_REG32(dr6);
+	DEFINE_REG32(dr7);
+	// TODO: GDTR, LDTR, IDTR, task, MTRR, MSR, MMX, SSE
+	/* Program counter (must be last) */
+	DEFINE_REG32(eip);
 });
 typedef struct reg_x86_s reg_x86_t;
 

libcpu/interface.cpp
@@ -40,8 +40,8 @@ extern arch_func_t arch_func_fapra;
 static inline bool
 is_valid_gpr_size(cpu_t *cpu, uint32_t offset, uint32_t count)
 {
-	for (; offset < count; offset++) {
-		size_t size = cpu->info.register_layout[offset].bits_size;
+	for (uint32_t idx = offset; idx < count + offset; idx++) {
+		size_t size = cpu->info.register_layout[idx].bits_size;
 		switch (size) {
 		case 0: case 1: case 8: case 16: case 32: case 64:
 			break;
@@ -55,8 +55,8 @@ is_valid_gpr_size(cpu_t *cpu, uint32_t offset, uint32_t count)
 static inline bool
 is_valid_fpr_size(cpu_t *cpu, uint32_t offset, uint32_t count)
 {
-	for (; offset < count; offset++) {
-		size_t size = cpu->info.register_layout[offset].bits_size;
+	for (uint32_t idx = offset; idx < count + offset; idx++) {
+		size_t size = cpu->info.register_layout[idx].bits_size;
 		switch (size) {
 		case 0: case 32: case 64: case 80: case 128:
 			break;
@@ -70,8 +70,8 @@ is_valid_fpr_size(cpu_t *cpu, uint32_t offset, uint32_t count)
 static inline bool
 is_valid_vr_size(cpu_t *cpu, uint32_t offset, uint32_t count)
 {
-	for (; offset < count; offset++) {
-		size_t size = cpu->info.register_layout[offset].bits_size;
+	for (uint32_t idx = offset; idx < count + offset; idx++) {
+		size_t size = cpu->info.register_layout[idx].bits_size;
 		switch (size) {
 		case 0: case 64: case 128:
 			break;
@@ -154,13 +154,13 @@ cpu_new(cpu_arch_t arch, uint32_t flags, uint32_t arch_flags)
 		offset += cpu->info.regclass_count[rc];
 	}
 
-	assert(is_valid_gpr_size(cpu, offset, cpu->info.regclass_offset[CPU_REGCLASS_GPR]) &&
+	assert(is_valid_gpr_size(cpu, cpu->info.regclass_offset[CPU_REGCLASS_GPR], cpu->info.regclass_count[CPU_REGCLASS_GPR]) &&
 		"the specified GPR size is not guaranteed to work");
-	assert(is_valid_gpr_size(cpu, offset, cpu->info.regclass_offset[CPU_REGCLASS_XR]) &&
+	assert(is_valid_gpr_size(cpu, cpu->info.regclass_offset[CPU_REGCLASS_XR], cpu->info.regclass_count[CPU_REGCLASS_XR]) &&
 		"the specified XR size is not guaranteed to work");
-	assert(is_valid_fpr_size(cpu, offset, cpu->info.regclass_offset[CPU_REGCLASS_FPR]) &&
+	assert(is_valid_fpr_size(cpu, cpu->info.regclass_offset[CPU_REGCLASS_FPR], cpu->info.regclass_count[CPU_REGCLASS_FPR]) &&
 		"the specified FPR size is not guaranteed to work");
-	assert(is_valid_vr_size(cpu, offset, cpu->info.regclass_offset[CPU_REGCLASS_VR]) &&
+	assert(is_valid_vr_size(cpu, cpu->info.regclass_offset[CPU_REGCLASS_VR], cpu->info.regclass_count[CPU_REGCLASS_VR]) &&
 		"the specified VR size is not guaranteed to work");
 
 